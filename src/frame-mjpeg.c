305b34fcecbf5af5c3e2807c43c50d33faf432b7
wangha <wanghamax@gmail.com>
2024-04-21 21:52:39 +0800

    add new interface uvc_mjpeg2bgr.

--- src/frame-mjpeg.c
+++ src/frame-mjpeg.c
@@ -173,6 +173,31 @@ fail:
   return UVC_ERROR_OTHER;
 }
 
+/** @brief Convert an MJPEG frame to BGR
+ * @ingroup frame
+ *
+ * @param in MJPEG frame
+ * @param out BGR frame
+ */
+uvc_error_t uvc_mjpeg2bgr(uvc_frame_t *in, uvc_frame_t *out) {
+  if (in->frame_format != UVC_FRAME_FORMAT_MJPEG)
+    return UVC_ERROR_INVALID_PARAM;
+
+  if (uvc_ensure_frame_size(out, in->width * in->height * 3) < 0)
+    return UVC_ERROR_NO_MEM;
+
+  out->width = in->width;
+  out->height = in->height;
+  out->frame_format = UVC_FRAME_FORMAT_BGR;
+  out->step = in->width * 3;
+  out->sequence = in->sequence;
+  out->capture_time = in->capture_time;
+  out->capture_time_finished = in->capture_time_finished;
+  out->source = in->source;
+
+  return uvc_mjpeg_convert(in, out);
+}
+
 /** @brief Convert an MJPEG frame to RGB
  * @ingroup frame
  *
